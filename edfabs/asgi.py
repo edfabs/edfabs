4dfcca234ea14db87604fdcc988d1129dc293d3a
Fabian Suchett <edgar.suchett@mrm-mccann.com>
2020-03-05 12:32:59 -0600

    first commit, add all needed files

---
+++ edfabs/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for edfabs project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edfabs.settings')
+
+application = get_asgi_application()
