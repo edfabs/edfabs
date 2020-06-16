02a4d299cb69a736e0bfe102f5138f2e68cbb649
Fabian Suchett <edgar.suchett@mrm-mccann.com>
2020-06-16 17:54:21 -0500

    Test pytesserat

--- projects/views.py
+++ projects/views.py
@@ -15,7 +15,11 @@ def index(request):
 
 def tesseract(request):
 	#print(pytesseract.image_to_string(Image.open('Escanear.jpg')))
+	#result = pytesseract.image_to_osd(Image.open(os.path.join(BASE_DIR, 'static/images/Escanear.jpeg')), config='--oem 3 --psm 6')
 	#print(pytesseract.image_to_string(Image.open(os.path.join(BASE_DIR, 'static/images/Escanear.jpeg'))))
-	result = pytesseract.image_to_string(Image.open(os.path.join(BASE_DIR, 'static/images/Escanear.jpeg')))
+	#custom_oem_psm_config = r'--oem 3 --psm 6'
+	#result = pytesseract.image_to_osd(Image.open(os.path.join(BASE_DIR, 'static/images/Escanear.jpeg')))
+	result = pytesseract.image_to_pdf_or_hocr(Image.open(os.path.join(BASE_DIR, 'static/images/Escanear.jpeg')), extension='hocr')
+	#result = result.replace('\n', '&nbsp')
 	return HttpResponse("result: {}".format(result))
 	#return render(request, 'projects/index.html', {"result: {}".format(result)})
