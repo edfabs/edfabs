ad3e9b471823125f84eac6548e71fe1169c3c3b9
Fabian Suchett <edgar.suchett@mrm-mccann.com>
2020-06-26 12:13:39 -0500

    change db configuration in settings file to run in production

--- edfabs/settings.py
+++ edfabs/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,16 +81,8 @@ WSGI_APPLICATION = 'edfabs.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv("db_name"),
-        'USER': os.getenv("db_user"),
-        'PASSWORD': os.getenv("password"),
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
