4f938cf409e0964a833d4a75715669e7a49143e2
Fabian Suchett <edgar.suchett@mrm-mccann.com>
2020-07-12 14:54:02 -0500

    comment masonry scrpts

--- static/js/js.js
+++ static/js/js.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){
-	$('.grid').imagesLoaded()
-	  .always( function( instance ) {
-	    console.log('all images loaded');
+	// $('.grid').imagesLoaded()
+	//   .always( function( instance ) {
+	//     console.log('all images loaded');
 	    
 		
-	  })
-	  .done( function( instance ) {
-	    console.log('all images successfully loaded');
-	    $('.grid').masonry({
-			initLayout: true,
-		 	itemSelector: '.grid-item',
-		 	columnWidth: '.grid-sizer',
-		 	percentPosition: true,
-		  	resize: true,
-		    stagger: 30,
-		 	fitWidth: true,
-		 	gutter: 10
-		});
-	  })
-	  .fail( function() {
-	    console.log('all images loaded, at least one is broken');
-	  })
-	  .progress( function( instance, image ) {
-	    var result = image.isLoaded ? 'loaded' : 'broken';
-	    console.log( 'image is ' + result + ' for ' + image.img.src );
-	  });
+	//   })
+	//   .done( function( instance ) {
+	//     console.log('all images successfully loaded');
+	//     $('.grid').masonry({
+	// 		initLayout: true,
+	// 	 	itemSelector: '.grid-item',
+	// 	 	columnWidth: '.grid-sizer',
+	// 	 	percentPosition: true,
+	// 	  	resize: true,
+	// 	    stagger: 30,
+	// 	 	fitWidth: true,
+	// 	 	gutter: 10
+	// 	});
+	//   })
+	//   .fail( function() {
+	//     console.log('all images loaded, at least one is broken');
+	//   })
+	//   .progress( function( instance, image ) {
+	//     var result = image.isLoaded ? 'loaded' : 'broken';
+	//     console.log( 'image is ' + result + ' for ' + image.img.src );
+	//   });
 	// $('.grid').on( 'click', '.grid-item', function() {
 	//   // remove clicked element
 	//   $('.grid').masonry()
