7650592a6840bb85681c57cd484cd8818da77d3d
Fabian Suchett <edgar.suchett@mrm-mccann.com>
2020-07-17 22:18:16 -0500

    add the Chioice section to admin

--- polls/views.py
+++ polls/views.py
@@ -24,7 +24,7 @@ def detail(request, question_id):
     return render (request, 'polls/detail.html', {'question': question})
 
 
-def result(request, question_id):
+def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
 
