4c57b50db1d5c6df78cd250920c9b649d5cdfb3d
Fabian Suchett <edgar.suchett@mrm-mccann.com>
2020-07-19 15:17:14 -0500

    Modify the was_published_recently of models to fix the error of published for the future and fix the error in test.py

--- polls/tests.py
+++ polls/tests.py
@@ -13,5 +13,5 @@ class QuestionModelTests(TestCase):
         is in the future
         """
         time = timezone.now() + datetime.timedelta(days=30)
-        future_question = question(pub_date=time)
+        future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
