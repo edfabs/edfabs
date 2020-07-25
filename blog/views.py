b7ef4b9e062aea4d9bf36d960cef4ef3951ec11d
Fabian Suchett <edgar.suchett@mrm-mccann.com>
2020-07-25 14:38:14 -0500

    add the section of blog, and nav bar of bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'latest_post_list'
+
+class DetailView(generic.DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
--- edfabs/urls.py
+++ edfabs/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-	path('', include('skeleton.urls')),
-	path('projects/', include('projects.urls')),
+    path('blog/', include('blog.urls')),
     path('polls/', include('polls.urls')),
+    path('projects/', include('projects.urls')),
+	path('', include('skeleton.urls')),
     path('admin/', admin.site.urls),
 ]
