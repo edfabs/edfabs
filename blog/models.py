cb29cd10eec7b5211b8b472307338d7d75a30b0b
Fabian Suchett <edgar.suchett@mrm-mccann.com>
2020-08-03 12:47:38 -0500

    change a type of slug date from slugfield to charfield

--- blog/models.py
+++ blog/models.py
@@ -20,7 +20,7 @@ class Category(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200)
+    slug = models.CharField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
