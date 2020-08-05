367d8d957b047e71f48bb573fc6e4efa2732bdb7
Fabian Suchett <edgar.suchett@mrm-mccann.com>
2020-08-04 19:01:45 -0500

    Change  User Pasword Page

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView
+from .views import UserRegisterView, UserEditView, PasswordsChangeViews
+from django.contrib.auth import views as auth_views
+from . import views
 
 app_name = 'members'
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
+    # path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change_password.html')),
+    path('password/', PasswordsChangeViews.as_view(template_name='registration/change_password.html')),
+    path('password_success', views.password_success, name="password_success"),
 ]
